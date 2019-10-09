01eb7986475cea750774689e19266ee71f77b4c3
Delache <delache33@gmail.com>
2019-10-09 16:12:14 +0200

    Experience carroussel

--- src/app/competence/competence.component.ts
+++ src/app/competence/competence.component.ts
@@ -12,4 +12,5 @@ export class CompetenceComponent implements OnInit {
   ngOnInit() {
   }
 
+
 }
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
+
 export class ExperienceComponent implements OnInit {
-  exp = [
+  scroll = 0;
+
+  experience = [
   {   title: 'Sapeur-pompier professionnel',
       society: 'Sdis33',
       date: 'septembre 2008 à septembre 2019',
+      // tslint:disable-next-line: max-line-length
       description: 'Sous-officier, chef d\'agrés (Médoc, Ornano) -- Chef opérateur de traitement de l\'alerte et de coordination opérationnelle -- Formateur, instructeur'},
 
     {	title: 'Caviste - Responsable chai à barriques',
@@ -41,5 +45,16 @@ export class ExperienceComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  next() {
+    if (this.scroll <  this.experience.length) {
+      this.scroll += 1;
+      console.log(this.scroll);
+    }
+  }
+  previous() {
+    if (this.scroll > 0) {
+      this.scroll -= 1;
+      console.log(this.scroll);
+    }
+  }
 }
